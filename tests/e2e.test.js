d68a7c3ab86143c59324f01fa59d520cd0317219
Martin <marb61a@gmail.com>
2020-09-13 18:42:38 +0100

    Updated e2e test

--- tests/e2e.test.js
+++ tests/e2e.test.js
@@ -6,6 +6,29 @@ describe("End to End test", () => {
     let page;
 
     before(async function() {
+        let browser = await puppeteer.launch({
+            headless: true
+        });
+
+        let page = await browser.newPage();
+        await page.setDefaultTimeout(10000);        
+    });
+
+    after(async function(){
+        await browser.close();
+    });
+
+    it('should submit form', async function(){
+        await page.goto('https://devexpress.github.io/testcafe/example/');
+        await page.waitForSelector('#main-form');
+        await page.type('#developer-name', 'Joe');
+        await page.click('#tried-test-cafe');
+        await page.click('#submit-button');
+        await page.waitForSelector('.result-content');
+    });
+
+    it('should select a value from the select box', async function(){
 
     });
+    
 });
