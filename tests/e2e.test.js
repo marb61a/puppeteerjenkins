1d19ec672d656725c0b6713156b6ea0917db3903
Martin <marb61a@gmail.com>
2020-09-13 18:35:37 +0100

    Updated smoke test and e2e test

---
+++ tests/e2e.test.js
@@ -0,0 +1,11 @@
+const puppeteer = require('puppeteer');
+
+// Using async in a describe block declaration will cause errors
+describe("End to End test", () => {
+    let browser;
+    let page;
+
+    before(async function() {
+
+    });
+});
--- tests/smoke.test.js
+++ tests/smoke.test.js
@@ -9,5 +9,11 @@ describe("Smoke Test", () => {
         let page = await browser.newPage();
         await page.setDefaultTimeout(10000);
 
+        // Using the Testcafe sample site
+        await page.goto('https://devexpress.github.io/testcafe/example/');
+        await page.waitForSelector('#main-form');
+
+        await browser.close();
+        
     });
 });
